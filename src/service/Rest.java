efe90e7e30579af673bac2cb163dbce7130c46ee
Tom Phily <tom.phily@live.fr>
2016-03-22 16:11:51 +0100

    ajout dun service rest

--- src/service/Rest.java
+++ src/service/Rest.java
@@ -7,7 +7,25 @@ import javax.ws.rs.core.Response;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
+@Path("/")
 public class Rest 
 {
+	public Response sendResponse(int status, String entity, String methode)
+	{
+		return Response
+				.status(status)
+				.entity(entity)
+				.header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With")
+                .header("Access-Control-Allow-Methods", methode)
+                .build();	
+	}
 	
+	@GET
+    @Path("/scan")
+    @Produces(MediaType.APPLICATION_JSON)
+	public Response scanCard()
+	{
+		return sendResponse(202, "", "PUT");
+	}
 }
