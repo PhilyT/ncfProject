b67f5b56f010b8365126d058067f7720f2085ff4
unknown <m.helie-zadeh@CZC0217zsw.mpm.fr>
2016-04-14 16:51:26 +0200

    debut config passport

---
+++ ClientNodeJs/passport.js
@@ -0,0 +1,50 @@
+// configuration du fichier de config passeport afin de permettre l'identification des professeurs
+// le fichier ce situ sous : config/passeport.js
+
+// prés requis 
+ var LocalStrategy = require ('passport-local').Strategy;
+ var mysql = require('mysql');
+
+ // donnée de connection à la base de donnée - A remplir
+
+ module.exports = function(passport) {
+
+ 	//===================================================
+ 	//==== Configuration des sessions ===================
+ 	//===================================================
+
+ 	// sérialisation et désérialisation des sessions utilisateurs
+ 	// sérialisation 
+ 	passport.serializeUser(function(user,done)
+ 		{
+ 			done(null, user.id);
+ 		});
+ 	//déserialisation
+ 	passport.deserializeUser(function(id,done)
+ 		{
+ 			connection.query("SELECT * FROM user WHERE id ="+id,function(err,rows)
+ 			{
+ 				done(err,rows[0]);
+ 			});
+ 		});
+
+ 	//===================================================
+ 	//==== Configuration connexion ======================
+ 	//===================================================
+
+ 	passport.use('local-signup', new LocalStrategy
+ 		(
+ 			{
+ 				usernameField:'email',
+ 				passwordField: 'password',
+ 				passReqToCallback : true
+ 			},
+ 			function (req, email, password, done) 
+ 			{
+ 				connection.query("SELECT * FROM")
+ 			}
+ 		)
+ 	)
+
+ }
+
