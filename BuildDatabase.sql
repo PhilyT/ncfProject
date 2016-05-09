380310b262f6abd7292a93e618ded76972fd6bda
Stéphanie Carrié <stephaniesmg@hotmail.fr>
2016-05-09 16:40:56 +0200

    Ajout de données pertinentes

--- BuildDatabase.sql
+++ BuildDatabase.sql
@@ -56,7 +56,13 @@ CREATE TABLE IF NOT EXISTS `presence` (
   CONSTRAINT `presence_ibfk_2` FOREIGN KEY (`idEtud`) REFERENCES `eleve` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+INSERT INTO cours VALUES('','9:00:00','14:00:00',"JEE");
+INSERT INTO cours VALUES('','14:00:00','17:00:00',"RFID");
+INSERT INTO eleve (prenom, nom, idCarte) VALUES('Tom','Phily','8B1D8B16');
+INSERT INTO eleve (prenom, nom, idCarte) VALUES('Marina','Helie-Zadeh','7B498C16');
+INSERT INTO eleve (prenom, nom, idCarte) VALUES('Stéphanie','Carrié','0BD09816');
+INSERT INTO presence (idEtud, idCours, Date) VALUES(1,1,'2016-05-09');
+INSERT INTO presence (idEtud, idCours, Date) VALUES(2,1,'2016-05-09');
 
  GRANT SELECT,UPDATE,INSERT ON rfid_badgeuse.* TO 'rfid'@'localhost' IDENTIFIED BY 'rfid' WITH MAX_QUERIES_PER_HOUR 100000 MAX_UPDATES_PER_HOUR 10000 MAX_CONNECTIONS_PER_HOUR 10000 MAX_USER_CONNECTIONS 10000;
  SET PASSWORD FOR 'rfid'@'localhost' = PASSWORD('rfid');
