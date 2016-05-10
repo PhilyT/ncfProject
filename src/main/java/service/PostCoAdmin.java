7cde9e336176dd60a80eba6e3933409b2bc739ae
lp-phily <tom.phily@live.fr>
2016-05-10 16:51:04 +0200

    ajout de route rest

--- src/main/java/service/PostCoAdmin.java
+++ src/main/java/service/PostCoAdmin.java
@@ -1,9 +1,28 @@
 package main.java.service;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jettison.json.JSONException;
+
 import main.java.models.*;
 
 public class PostCoAdmin extends HttpServlet
 {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+		String mailAdmin = request.getParameter("email");
+		
+    }
+	
+	private void setResponse(HttpServletResponse response) throws JSONException, IOException
+	{
+		
+	}
+	
 	
 }
