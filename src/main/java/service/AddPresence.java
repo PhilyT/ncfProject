cd275505e0b8ca7d31c05ac56f6783c6e0357c04
Tom Phily <tom.phily@live.fr>
2016-05-12 03:07:42 +0200

    ajout de la route addpresence du servicerest

--- src/main/java/service/AddPresence.java
+++ src/main/java/service/AddPresence.java
@@ -1,7 +1,10 @@
 package main.java.service;
 
 import java.io.IOException;
+import java.sql.Date;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
+import main.java.models.Presence;
 import main.java.moteur.ConnectionBD;
 
 public class AddPresence extends HttpServlet 
@@ -33,7 +37,18 @@ public class AddPresence extends HttpServlet
 		JSONObject json = new JSONObject();
 		try
 		{
+			SimpleDateFormat sdf1 = new SimpleDateFormat("dd-mm-yyyy");
+			int idcour = Integer.parseInt(id_c);
+			int idetud = Integer.parseInt(idEtud);
+			java.util.Date date1 = sdf1.parse(date);
+			Date sqlStartDate = new Date(date1.getTime());
 			ConnectionBD maco = new ConnectionBD();
+			Presence presence = new Presence();
+			presence.setDate(sqlStartDate);
+			presence.setIdCours(idcour);
+			presence.setIdEtud(idetud);
+			maco.insertPresence(presence);
+			json.put("etat", "success");
 			response.setStatus(200);
 	        response.setContentType("application/json");
 	        response.getWriter().write(json.toString());
@@ -41,6 +56,12 @@ public class AddPresence extends HttpServlet
 		catch(SQLException e)
 		{
 			json.put("etat", "Erreur accès base de données !");
+			response.setStatus(200);
+	        response.setContentType("application/json");
+	        response.getWriter().write(json.toString());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			json.put("etat", "Erreur de parsing de date !");
 			response.setStatus(200);
 	        response.setContentType("application/json");
 	        response.getWriter().write(json.toString());
